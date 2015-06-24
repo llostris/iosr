05ae025215760c632d51e2664f470672f414564c
Llostris <deiidrell@gmail.com>
2015-06-24 22:11:22 +0200

    refactoring of tests, added files specific to hdfs jobs

--- test.sh
+++ test.sh
@@ -1,16 +1,34 @@
 #!/bin/bash -e
 
-function start {
-        APP_NAME=$1
-        rhc app create $APP_NAME diy-0.1 
-        rhc cartridge add https://raw.githubusercontent.com/llostris/iosr/cartridge/metadata/manifest.yml -a $APP_NAME
+DIRECTORY=$3
+
+function create {
+	APP_NAME=$1
+	rhc app create $APP_NAME diy-0.1 
+	rhc cartridge add https://raw.githubusercontent.com/llostris/iosr/cartridge/metadata/manifest.yml -a $APP_NAME
+}
+
+function run {
+	if [ -z $DIRECTORY ]
+	then
+		echo "No directory provided"
+		exit 1
+	fi
+	
+	APP_NAME=$1
 	cd $APP_NAME
-        cp ../manifest.yml .
-	cp ../SparkPi.jar .
-        git add manifest.yml SparkPi.jar
-        git commit -m "init"
+	cp ../$DIRECTORY/manifest.yml .
+	cp ../binaries/SparkPi.jar .
+	cp ../binaries/wordcount.jar .
+	git add .
+	git commit -m "init"
 	git push origin master
 	cd ..
+}		
+
+function start {
+	create $1
+	run $1
 }
 
 function stop {
@@ -25,8 +43,10 @@ then
 	case "$2" in
 		start)		start $1 ;;
 		stop)		stop $1 ;;
-		*)		echo "Invalid action provided"
-				exit 0;;
+		create)		create $1 ;;
+		run)		run $1 ;;
+		*)			echo "Invalid action provided"
+		exit 0;;
 	esac
 else
 	echo "No app name provided"
